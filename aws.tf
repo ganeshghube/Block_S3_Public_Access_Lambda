6a027c76fd1a3736f8185dfc956c9e6334f0a435
Ganesh Ghube <ganesh.ghube@gmail.com>
2023-02-09 14:43:08 +0000

    Update aws.tf

--- aws.tf
+++ aws.tf
@@ -1,13 +1,5 @@
 terraform {
   #required_version = "~> 0.12.0"
-
-#  backend "s3" {
-#    acl     = "bucket-owner-full-control"
-#    bucket  = "tf-state-cdcloudlogix"
-#    encrypt = true
-#    key     = "lambda_s3_public.tfstate"
-#    region  = "eu-west-1"
-#  }
 }
 
 provider "aws" {
